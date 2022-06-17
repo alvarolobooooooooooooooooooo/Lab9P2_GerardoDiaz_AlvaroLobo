8556590eee8183606caa79a85677b622e45faea7
alvarolobooooooooooooooooooo <105078434+alvarolobooooooooooooooooooo@users.noreply.github.com>
2022-06-17 16:05:49 -0600

    se tuvo que comenzar desde 0

---
+++ src/Archivo.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agln7
+ */
+public class Archivo {
+   private String nombre;
+   private String link;
+   private String extension;
+   private int tamano;
+   private boolean destacado;
+
+    public Archivo(String nombre, String link, String extension, int tamano) {
+        this.nombre = nombre;
+        this.link = link;
+        this.extension = extension;
+        this.tamano = tamano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + extension;
+    }
+}
---
+++ src/Carpeta.java
@@ -0,0 +1,66 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agln7
+ */
+public class Carpeta {
+    private String nombre;
+    private String link;
+    private ArrayList <Archivo> archivos = new ArrayList();
+    private ArrayList <Carpeta> carpetas = new ArrayList();
+    private boolean destacado;
+
+    public Carpeta() {
+    }
+
+    
+    public Carpeta(String nombre, String link) {
+        this.nombre = nombre;
+        this.link = link;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ArrayList<Archivo> getArchivos() {
+        return archivos;
+    }
+
+    public void setArchivos(ArrayList<Archivo> archivos) {
+        this.archivos = archivos;
+    }
+
+    public ArrayList<Carpeta> getCarpetas() {
+        return carpetas;
+    }
+
+    public void setCarpetas(ArrayList<Carpeta> carpetas) {
+        this.carpetas = carpetas;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    } 
+}
